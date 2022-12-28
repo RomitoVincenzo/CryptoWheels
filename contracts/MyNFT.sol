6f805651f2a8f0ebd6051cc1f11b4ce3947808f7
Vincenzo Romito <romito_vincenzo@libero.it>
2022-12-28 17:39:53 +0100

    added fetch my nfts

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -6,26 +6,35 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract CryptoWheels is ERC721, ERC721URIStorage, Ownable {
+contract CryptoWheels is ERC721, ERC721URIStorage {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
+    address payable contractOwner;
 
     mapping(string => uint8) existingURIs;
+    mapping(uint256 => address) private idToOwner;
 
-    constructor() ERC721("CryptoWheels", "CW") {}
+    /*event MarketItemCreated (
+      uint256 indexed tokenId,
+      address owner
+    );*/
 
-    function _baseURI() internal pure override returns (string memory) {
-        return "ipfs://";
+    constructor() ERC721("CryptoWheels", "CW") {
+        contractOwner = payable(msg.sender);
     }
 
-    function safeMint(address to, string memory uri) public onlyOwner {
+    function _baseURI() internal pure override returns (string memory) {
+        return "";
+    }
+
+    /*function safeMint(address to, string memory uri) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
         existingURIs[uri] = 1;
-    }
+    }*/
 
     // The following functions are overrides required by Solidity.
 
@@ -50,9 +59,8 @@ contract CryptoWheels is ERC721, ERC721URIStorage, Ownable {
     }
 
     function payToMint(
-        //added by fireship
         address recipient,
-        string memory metadataURI
+        string memory metadataURI //json cid
     ) public payable returns (uint256) {
         require(existingURIs[metadataURI] != 1, "NFT already minted!");
         require(msg.value >= 0.05 ether, "Need to pay up!");
@@ -63,8 +71,30 @@ contract CryptoWheels is ERC721, ERC721URIStorage, Ownable {
 
         _mint(recipient, newItemId);
         _setTokenURI(newItemId, metadataURI);
+        idToOwner[newItemId] = recipient;
 
         return newItemId;
+    }
+
+    function fetchMyNFTs(address adr) public view returns (uint256[] memory) {
+        uint256 totalItemCount = _tokenIdCounter.current();
+        uint256 itemCount = 0;
+        uint256 currentIndex = 0;
+
+        for (uint256 i = 0; i < totalItemCount; i++) {
+            if (idToOwner[i] == adr) {
+                itemCount += 1;
+            }
+        }
+        uint256[] memory items = new uint256[](totalItemCount);
+        for (uint256 i = 0; i < totalItemCount; i++) {
+            if (idToOwner[i] == adr) {
+                uint256 currentId = i;
+                items[currentIndex] = currentId;
+                currentIndex += 1;
+            }
+        }
+        return items;
     }
 
     function count() public view returns (uint256) {
