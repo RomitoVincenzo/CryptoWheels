bd7b487f8cec5ae8578a67f394081ff5825457f8
Giacomo Zarba Meli <giacomo.zarba@immobiliare.it>
2022-12-30 19:35:27 +0100

    add navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Nav, Navbar } from 'react-bootstrap';
 
 const Home = () => {
 return (
-<div>
-<h1>Home</h1>
-<Link to="/unbox">
-<button>Unbox</button>
-</Link>
-<Link to="/garage">
-<button>Garage</button>
-</Link>
-</div>
+    <div style={{ backgroundColor: 'gray' }} >
+      <Navbar style={{ fontSize: "2.5rem" }} fixed="top" bg="dark" variant="dark" expand="sm">
+        <Navbar.Brand style={{ fontSize: "2.5rem", marginLeft: "1rem" }} href="/">Home</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link style={{ marginLeft: "1rem" }} href="/unbox">Unbox</Nav.Link>
+            <Nav.Link style={{ marginLeft: "1rem" }} href="/garage">Garage</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
 );
 };
 
